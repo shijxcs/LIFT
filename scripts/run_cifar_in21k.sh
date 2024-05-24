952cf3967d1d1b1193e8cad8c4dc30a5f21b487f
Jiang-Xin Shi <shijx@lamda.nju.edu.cn>
2024-05-24 19:49:26 +0800

    Update run_cifar_in21k.sh

--- scripts/run_cifar_in21k.sh
+++ scripts/run_cifar_in21k.sh
@@ -1,4 +1,4 @@
-# PEL with pre-trained ViT from ImageNet-21K
+# LIFT with pre-trained ViT from ImageNet-21K
 
 python main.py -d cifar100_ir100 -m in21k_vit_b16_peft init_head class_mean test_ensemble False
 python main.py -d cifar100_ir50 -m in21k_vit_b16_peft init_head class_mean test_ensemble False
